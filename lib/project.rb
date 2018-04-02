f2e7e234959a63c233d2f74d39d1380c30948bf1
Lauren Perchitti <laurenperchitti@gmail.com>
2018-04-02 15:47:47 +0000

    automatically backed up by learn

--- lib/project.rb
+++ lib/project.rb
@@ -1,9 +1,9 @@
 class Project
 
-  attr_accessor :title
+  attr_accessor :title, :backers
 
-  def initialize(name)
+  def initialize(title)
+  @title = title
     @backers = []
-    @title = title
   end
 end
